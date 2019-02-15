747506e787066dfb7c378208e296b97423b0a57f
Saswat1999 <47583914+Saswat1999@users.noreply.github.com>
2019-02-15 11:13:35 +0530

    Create updatedata.php

---
+++ updatedata.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "inno";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "UPDATE rawdata SET flag='$flag' WHERE location='$point'";
+
+if ($conn->query($sql) === TRUE) {
+    echo "	data ";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+?>
